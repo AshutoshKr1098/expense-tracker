e28aa14a79d707a9a84c2604adc5e7c7080f5bb9
Ashutosh <ashu.heretocode@gmail.com>
2021-06-27 16:03:00 +0530

    Added Income and Expense reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,26 @@
 import { combineReducers } from "redux";
 
-const dummyReducer = () => {
-  return null;
+const income = (state = null, action) => {
+  switch (action.type) {
+    case "GET_INCOME":
+      return action.payload;
+    case "ADD_INCOME":
+      return action.payload;
+    default:
+      return state;
+  }
+};
+const expense = (state = [], action) => {
+  switch (action.type) {
+    case "GET_EXPENSE":
+      return action.payload;
+    case "GET_EXPENSE_DETAIL":
+      return action.payload;
+    case "ADD_EXPENSE":
+      return [...state, action.payload];
+  }
 };
 export default combineReducers({
-  dummyReducer: dummyReducer,
+  income: income,
+  expense: expense,
 });
