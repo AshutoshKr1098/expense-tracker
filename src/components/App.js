1ed22a022f278d26d2960c9742f7d9d726d939ad
Ashutosh <ashu.heretocode@gmail.com>
2021-06-27 15:31:28 +0530

    styled expense detail area

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ExpenseForm from "./ExpenseForm";
+import ExpenseDetail from "./ExpenseDetail";
 import "../assets/style.css";
 class App extends Component {
   render() {
@@ -7,7 +8,10 @@ class App extends Component {
       <div className="app">
         <h2>Expense Tracker App</h2>
         <hr />
-        <ExpenseForm />
+        <div className="app_body">
+          <ExpenseForm />
+          <ExpenseDetail />
+        </div>
       </div>
     );
   }
